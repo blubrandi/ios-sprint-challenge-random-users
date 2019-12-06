03c6172728f4a888fffe0e589f3b0d45a47525aa
blubrandi <blubrandi0@gmail.com>
2019-12-06 09:44:25 -0700

    Created cache file

---
+++ Random Users/Model/Cache.swift
@@ -0,0 +1,27 @@
+//
+//  Cache.swift
+//  Random Users
+//
+//  Created by Brandi on 12/6/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+
+class Cache<Key: Hashable, Value> {
+    
+    private var cache = [Key : Value]()
+    private var queue = DispatchQueue(label: "com.LambdaSchool.RandomUsers.ConcurrentOperationStateQueue")
+    
+    func cache(key: Key, value: Value) {
+        queue.async {
+            self.cache[key] = value
+        }
+    }
+    
+    func calue(key: Key) -> Value? {
+        return queue.sync {
+            cache[key]
+        }
+    }
+}
---
+++ Random Users/Model/Testing.swift
@@ -0,0 +1,9 @@
+//
+//  Testing.swift
+//  Random Users
+//
+//  Created by Brandi on 12/6/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import Foundation
